732fed5088aea4363858d164674aea22e0a9a6ad
Harsh Modi <harshmodi@Harshs-MacBook-Pro.local>
2025-12-25 12:30:34 -0700

    Cancelled the deployment and reverted the changes

--- app/db.py
+++ app/db.py
@@ -1,16 +1,17 @@
 import psycopg2
-from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
 
-# Load variables from .env file
 load_dotenv()
 
 def get_db_connection():
     try:
         conn = psycopg2.connect(
-            os.getenv("DATABASE_URL"), # We will use just one URL variable now
-            sslmode='require'          # <--- IMPORTANT for cloud DBs
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
         )
         return conn
     except Exception as e:
